5f616af91188c82036aadbac8d8bfc8e8603648c
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2024-12-09 00:16:34 +0500

    export with capital letters

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import Link from "next/link";
 export default function Cart() {
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "./page-components/Hero";
 import Listing from "./page-components/Listing";
 import Listing2 from "./page-components/Listing2";
 import SignUp from "./page-components/SignUp";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
